13d6799323b8c9f47460638cfd22017cc9604799
dmunofer <diego.fmz@hotmail.com>
2021-12-12 19:26:10 +0100

    final commit

---
+++ Tarea1.py
@@ -0,0 +1,31 @@
+def ajedrez(filename):
+
+    tablero_inicial= '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
+    tablero = []
+
+    for e in tablero_inicial.split('\n'):
+        tablero.append(e.split('\t'))
+
+    f = open(filename,'w')
+    for e in tablero:
+        f.write('\t'.join(e)+'\n')
+    f.close()
+
+    movimientos = 0
+    while True:
+        continua = input('Quieres hacer otro movimiento (si/no) : ')
+        if continua == 'si':
+            fila_origen = int(input('Introduce la fila de la pieza: '))
+            columna_origen = int(input('Introduce la columna de la pieza: '))
+            fila_destino = int(input('Introduce la fila destino: '))
+            columna_destino = int(input('Introduce la columna destino: '))
+            tablero[fila_destino-1][columna_destino-1] = tablero[fila_origen-1][columna_origen-1]
+            tablero[fila_origen-1][columna_origen-1] = ' '
+            movimientos+=1
+            f=open(filename,'w')
+            f.write('Movimiento'+ f'{movimientos}' + '\n')
+
+            for e in tablero:
+                f.write('\t'.join(e)+'\n')
+            f.close()
+
